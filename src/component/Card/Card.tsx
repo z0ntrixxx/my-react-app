5e86a31fabc3a80b050fefa10ebdad29e2393307
z0ntrixxx <dsanirodari@gmail.com>
2025-06-26 13:33:59 +0000

    Переделал задание 3 по Вашему комментарию

--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -7,8 +7,8 @@ interface CardProps {
 export function Card ({id, title, body}: CardProps){
     return (
     <a className={`creator__item creator__item-${id}`} href="#">
-        <h3 class="creator__name">{title}</h3>
-        <p class="creator__description">{body}</p>
+        <h3 className="creator__name">{title}</h3>
+        <p className="creator__description">{body}</p>
         </a>
        ) 
 }
--- src/component/CardList/CardList.tsx
+++ src/component/CardList/CardList.tsx
@@ -1,22 +1,41 @@
-import {useState, useEffect} from 'react'
-import {Card} from '../Card/Card'
+import { useState, useEffect } from 'react';
+import { Card } from '../Card/Card';
 
 interface CardProps {
-    id: number;
-    title: string;
-    body: string;
+  id: number;
+  title: string;
+  body: string;
 }
 
 export function CardList() {
-const [cards, setCards] = useState<CardProps[]>([]);
+  const [cards, setCards] = useState<CardProps[]>([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=4 ')
-    .then(response => response.json())
-    .then(data => setCards(data));
-}, []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=4')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setCards(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
 
- return (
+  if (loading) {
+    return <div>Загрузка карточек...</div>;
+  }
+
+  if (error) {
+    return <div>Ошибка: {error}</div>;
+  }
+
+  return (
     <div className="creator__list">
       {cards.map(card => (
         <Card
@@ -29,4 +48,3 @@ useEffect(() => {
     </div>
   );
 }
-
