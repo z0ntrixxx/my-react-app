143873fc4d769560fdded05b95a1f3696d1dd7c8
z0ntrixxx <dsanirodari@gmail.com>
2025-07-07 21:33:31 +0000

    Доделано

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Header } from '../../component/Header/Header'
+import { Form } from '../../component/Form/Form'
 
-export const NotFoundPage = ():React.ReactElement => {
+export const NotFoundPage = (): React.ReactElement => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <h1 className='Not__found'>404 Not found page</h1>
+        <div className='Not__found'>
+            <Header openForm={() => setIsOpen(true)} />
+            <div className='Not__found-page'>
+                <h1 className='Not__found'>404 Not found page</h1>
+                <button className='Not__found__button'><NavLink className='Not__found__button__link' to='/'>Вернуться</NavLink></button>
+            </div>
+            <Form isOpen={isOpen} onClose={() => setIsOpen(false)} />
+        </div>
     )
 }
